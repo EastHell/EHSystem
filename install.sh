e2789eb23cfd34e5b8084f55f9db9df580acdc9c
Aleksandr Shushkov <djturbo9393@gmail.com>
2022-07-27 23:49:40 +0300

    fix home directory

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 SYSTEM_REMOTE_URL="https://github.com/EastHell/System.git"
-SYSTEM_LOCAL_PATH="~/Documents/System1"
+SYSTEM_LOCAL_PATH="{$HOME}/Documents/System1"
 
 git clone "$SYSTEM_REMOTE_URL" "$SYSTEM_LOCAL_PATH"
