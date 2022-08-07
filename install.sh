080d478e122c73dc7d438b7c3256f293191b48fe
Aleksandr Shushkov <djturbo9393@gmail.com>
2022-08-07 18:35:38 +0300

    Add PATH export

--- install.sh
+++ install.sh
@@ -3,4 +3,18 @@
 SYSTEM_REMOTE_URL="https://github.com/EastHell/System.git"
 SYSTEM_LOCAL_PATH="${HOME}/.system"
 
+if [ -d ${SYSTEM_LOCAL_PATH} ]
+then
+  echo "${SYSTERM_LOCAL_PATH} already exist."
+  exit
+fi
+
 git clone "$SYSTEM_REMOTE_URL" "$SYSTEM_LOCAL_PATH"
+
+if [[ ":${PATH}:" != *":${SYSTEM_LOCAL_PATH}:"* ]]
+then
+  echo "${SYSTEM_LOCAL_PATH} already exist in $PATH variable"
+else
+  echo "export PATH=${PATH}:${SYSTEM_LOCAL_PATH}" >> ${HOME}/.zshrc
+  export PATH=${PATH}:${SYSTEM_LOCAL_PATH}
+fi
