79f3c184715ce24a90979ad019befacc995fb2ed
Aleksandr Shushkov <djturbo9393@gmail.com>
2022-08-14 00:40:20 +0300

    Remove rendundant escape symbol

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ git clone "$SYSTEM_REMOTE_URL" "$SYSTEM_LOCAL_PATH"
 
 if [[ ":${PATH}:" != *":~/.EHSystem:"* ]]
 then
-  echo -n 'export PATH=\$PATH:~/.EHSystem' >> ${HOME}/.zshrc
+  echo -n 'export PATH=$PATH:~/.EHSystem' >> ${HOME}/.zshrc
   source $SYSTEM_LOCAL_PATH/.EHSEnviroment
 else
   echo "${SYSTEM_LOCAL_PATH} already exist in $PATH variable"
