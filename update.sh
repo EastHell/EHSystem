34353093f4384b15954c1267a02efa5b521a7f9a
Aleksandr Shushkov <djturbo9393@gmail.com>
2022-08-18 17:17:20 +0300

    Add p10k configuration

--- update.sh
+++ update.sh
@@ -11,6 +11,10 @@ source ${SCRIPT_DIR}/omz_install.sh
 # Install brew
 source ${SCRIPT_DIR}/brew_install.sh
 
+# Add p10k configurations
+rm ~/.p10k.zsh
+cp ~/.EHSystem/.p10k.zsh ~/.p10k.zsh
+
 # Replace .zshrc with new one
 rm ~/.zshrc
 cp ~/.EHSystem/.zshrc ~/.zshrc
