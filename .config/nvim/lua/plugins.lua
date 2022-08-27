94ee83c2ccc8054b13d58bab516275d8b5e5cf41
Aleksandr Shushkov <djturbo9393@gmail.com>
2022-08-27 21:12:17 +0300

    Add coc plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -29,6 +29,8 @@ return require('packer').startup(function(use)
         end
     }
 
+    use 'neoclide/coc.nvim'
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
