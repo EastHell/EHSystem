0f8c1291b28364a9bf530ef8aaa3cc7b6b16f79f
Aleksandr Shushkov <djturbo9393@gmail.com>
2022-08-29 21:44:42 +0300

    Add feline activation, and telescope mappings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
--- [[ options ]] ---
+--[[ options ]]--
 vim.opt.number = true -- shows line numbers
 vim.opt.ignorecase = true -- make find ignoring case
 vim.opt.smartcase = true -- make find ignoring case if no CAPS symbols entered
@@ -8,3 +8,13 @@ vim.opt.expandtab = true -- use spaces instead of tab
 vim.opt.smarttab = true -- indent/dedent in leading whitespaces
 
 require('plugins')
+
+--[[ feline ]]--
+require('feline').setup()
+require('feline').winbar.setup()
+
+--[[ telescope ]]--
+nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
+nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
+nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
